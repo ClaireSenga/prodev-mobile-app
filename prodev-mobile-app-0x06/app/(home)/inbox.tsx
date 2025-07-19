be43aabba5318b80b558ab80dfc4b0db6f89f88b
Infdesk025 <infdesk025@Infdesk025s-MacBook-Air.local>
2025-07-19 18:59:53 +0200

    2

---
+++ prodev-mobile-app-0x06/app/(home)/inbox.tsx
@@ -0,0 +1,12 @@
+// Example for saved.tsx, repeat this in inbox, search, profile
+import { View, Text } from "react-native";
+
+const ScreenName = () => {
+  return (
+    <View>
+      <Text>Screen Name</Text>
+    </View>
+  )
+}
+
+export default ScreenName;
